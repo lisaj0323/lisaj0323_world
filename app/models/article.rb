38463fe50083f416c3e0e63a14bc94c5967c3efd
Steve Cho <sungbumc@andrew.cmu.edu>
2014-02-22 23:58:12 -0500

    done

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
 	belongs_to :category
 
 	scope :active, where('active = ?', true)
-  	scope :alphabetical, order('name')
+  	scope :alphabetical, order('title')
 
 end
